aecd330a5661bb83a36777b45db13ffc40977887
Asturuf2 <02biglernoah@gmail.com>
2023-09-20 13:37:52 -0500

    can add two expressions

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ button.forEach(element => {
     element.addEventListener("click", (e) =>{
         const value = e.target.dataset.value
         const string = header.innerText
-       
+        // if equal sign is pressed
         if (value == "="){
             if(header.innerText.includes('/')){
                 const split = header.innerText.split('/')
@@ -28,15 +28,122 @@ button.forEach(element => {
 
             }
         } else {
+         // if a operator symbol is pressed a second time
+         // if string has 2 operators, perform the firt operator and add the second one to the answer
          header.innerText += value;
-         const string = header.innerText; 
-         const conditions = ["/", "+", "-", "x"]; 
-            if (string.includes("/") && string.charAt(string.length-1) == "+"){//((string.includes("/") || string.includes("+") || string.includes("-") || string.includes("x")) && (string.charAt(lastChar) == "/" || string.charAt(lastChar) == "/" || string.charAt(lastChar) == "-" || string.charAt(lastChar) == "+")){
-                const removeChar = string.slice(0,-1); console.log(removeChar)
-                const split = removeChar.split("/"); console.log(split)
-                const symbol = string.charAt(string.length-1); console.log(symbol)
-                operator("/",split[0], split[1])
-                header.innerText += symbol
+         const newString = header.innerText;
+         const symbol = newString.charAt(newString.length-1)
+        // if the last char is a symbol && the string includes another symbol
+            if (newString.charAt(newString.length-1)=="/"){
+                const removeChar = newString.slice(0,-1)
+                if(removeChar.includes('/')){
+                    const split = removeChar.split('/')
+                    operator("/", split[0], split[1])
+                    header.innerText += symbol
+                }
+    
+                if(removeChar.includes('x')){
+                    const split = removeChar.split('x')
+                    operator("x", split[0], split[1])
+                    header.innerText += symbol
+    
+                }
+                if(removeChar.includes('-')){
+                    const split = removeChar.split('-')
+                    operator("-", split[0], split[1])
+                    header.innerText += symbol
+    
+                }
+                if(removeChar.includes('+')){
+                    const split = removeChar.split('+')
+                    operator("+", split[0], split[1])
+                    header.innerText += symbol
+    
+                }
+            }
+            if (newString.charAt(newString.length-1)=="+"){
+                const removeChar = newString.slice(0,-1)
+                if(removeChar.includes('/')){
+                    const split = removeChar.split('/')
+                    operator("/", split[0], split[1])
+                    header.innerText += symbol
+                }
+    
+                if(removeChar.includes('x')){
+                    const split = removeChar.split('x')
+                    operator("x", split[0], split[1])
+                    header.innerText += symbol
+    
+                }
+                if(removeChar.includes('-')){
+                    const split = removeChar.split('-')
+                    operator("-", split[0], split[1])
+                    header.innerText += symbol
+    
+                }
+                if(removeChar.includes('+')){
+                    const split = removeChar.split('+')
+                    operator("+", split[0], split[1])
+                    header.innerText += symbol
+    
+                }
+                
+            }
+            if (newString.charAt(newString.length-1)=="-"){
+                const removeChar = newString.slice(0,-1)
+                if(removeChar.includes('/')){
+                    const split = removeChar.split('/')
+                    operator("/", split[0], split[1])
+                    header.innerText += symbol
+                }
+    
+                if(removeChar.includes('x')){
+                    const split = removeChar.split('x')
+                    operator("x", split[0], split[1])
+                    header.innerText += symbol
+    
+                }
+                if(removeChar.includes('-')){
+                    const split = removeChar.split('-')
+                    operator("-", split[0], split[1])
+                    header.innerText += symbol
+    
+                }
+                if(removeChar.includes('+')){
+                    const split = removeChar.split('+')
+                    operator("+", split[0], split[1])
+                    header.innerText += symbol
+    
+                }
+                
+            }
+            if (newString.charAt(newString.length-1)=="x"){
+                const removeChar = newString.slice(0,-1)
+                if(removeChar.includes('/')){
+                    const split = removeChar.split('/')
+                    operator("/", split[0], split[1])
+                    header.innerText += symbol
+                }
+    
+                if(removeChar.includes('x')){
+                    const split = removeChar.split('x')
+                    operator("x", split[0], split[1])
+                    header.innerText += symbol
+    
+                }
+                if(removeChar.includes('-')){
+                    const split = removeChar.split('-')
+                    operator("-", split[0], split[1])
+                    header.innerText += symbol
+    
+                }
+                if(removeChar.includes('+')){
+                    const split = removeChar.split('+')
+                    operator("+", split[0], split[1])
+                    header.innerText += symbol
+    
+                }
+                
             }
         }
     })
@@ -50,10 +157,10 @@ clear.addEventListener("click", () => {
 const operator = (x, y, z) => {
     let numOne = Number(y)
     let numTwo = Number(z)
-
-    if(x == "/" ){
+    if(x == "/" ) {
         let value = (numOne/numTwo)
-        header.innerText = value
+        let roundValue = Math.round(value*100)/100
+        header.innerText = roundValue
     } else if (x == "x"){
         let value = (numOne*numTwo)
         header.innerText = value
@@ -68,12 +175,5 @@ const operator = (x, y, z) => {
 
 }
 
-//if header.innerText.includes ( / or + or x or - )
-
-    //if an operator is clicked check to see if there is already an operate 
-    //if so, compute the prior expression and set the result as num 1
-    //if not 
-
-    // if (header.innerText.includes("/", "+", "-", "x") || value == "=")
-    //if (value = +, /, -, x) && header.innerText.includes("/", "+", "-", "x"))
-    // (value = "+" || value = "/" || value = "x" || value = "-" && header.innerText.includes("/", "+", "-", "x"))
+//create a conditional statement that doesn't let you click two operators in a row 
+//if lastchar = symbol 
